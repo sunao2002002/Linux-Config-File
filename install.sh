0b95629d3045c9e344c40d479aa8b7df64fecd9c
David Sun <sunao2002002@gmail.com>
2015-10-25 20:22:13 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 PLATFORM=$(uname)
 function mybackup(){
-    cp $1 $1_bak
+    cp -vf $1 $1_bak
 }
 if [ "$PLATFORM" = "Darwin" -or "$PLATFORM" = "Linux" ];then
     echo "install on $PLATFORM" 
     mybackup ~/.vimrc
-    cp ./bashrc ~/.bashrc
-    cp ./bash_profile ~/.bash_profile
-    cp ./vimrc ~/.vimrc
+    cp -vf ./bashrc ~/.bashrc
+    cp -vf ./bash_profile ~/.bash_profile
+    cp -vf ./vimrc ~/.vimrc
 else
     echo "Unsupported platform"
 fi
  
 if [ "$PLATFORM" = "Darwin"  ];then
     mybackup ~/.bash_profile
-    cp ./bash_profile ~/.bash_profile
+    cp -vf ./bash_profile ~/.bash_profile
 fi
