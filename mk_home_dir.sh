f346d0703bba12d6eb35057840d59e94a0e22fed
david.sun <sunao2002002@gmail.com>
2019-02-11 00:12:19 +0800

    setup home dirs

---
+++ mk_home_dir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd  ~
+ln -s "下载" Download
+ln -s "公共的" Public
+ln -s "图片"  Picture
+ln -s "文档" Document
+ln -s "桌面" Desktop
+
+ln -s "模板" Template
+ln -s "视频" Video
+ln -s "音乐" Music
+
